09af291a90dd39ad5f45b890dc4d4adf268d896c
fatoudiallo <fatous.diallo@yahoo.fr>
2022-07-05 16:47:55 +0200

    ss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_152653) do
+ActiveRecord::Schema.define(version: 2022_07_05_141919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2022_07_04_152653) do
     t.integer "price_per_day"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "item_photo"
   end
 
   create_table "reviews", force: :cascade do |t|
